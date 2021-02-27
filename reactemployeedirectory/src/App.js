3c8e5bba5d7298101579daf2a973bd16593bd20c
EsmondKim <ekelectronicmail@gmail.com>
2021-02-27 17:04:06 -0600

    Added dummy pages and API.js.

--- reactemployeedirectory/src/App.js
+++ reactemployeedirectory/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import About from "./components/About/About.js";
+import Discover from "./components/Discover/Discover.js";
+import Search from "./components/Search/Search.js";
+import NavTabs from "./components/NavBar/NavBar.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavTabs />
+      <Route exact path="/" component={About} />
+      <Route exact path="/discover" component={Discover} />
+      <Route exact path="/search" component={Search} />
+    </Router>
   );
 }
 
