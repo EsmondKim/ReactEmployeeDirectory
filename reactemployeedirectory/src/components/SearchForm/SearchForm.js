5ba4881c658a13a670783a579ca0d2eebfb392e3
EsmondKim <ekelectronicmail@gmail.com>
2021-03-08 01:56:19 -0600

    Added UI/UX instructions for user and renamed Search button to Refresh.

--- reactemployeedirectory/src/components/SearchForm/SearchForm.js
+++ reactemployeedirectory/src/components/SearchForm/SearchForm.js
@@ -6,24 +6,29 @@ function SearchForm(props) {
   const handleInputChange = (e) => {
     setInput(e.target.value);
     let lC = e.target.value.toLowerCase();
-    let newArr = props.employees.filter(employee => employee.firstName.toLowerCase().indexOf(lC) !== -1 );
+    let newArr = props.employees.filter(
+      (employee) => employee.firstName.toLowerCase().indexOf(lC) !== -1
+    );
     props.setTable(newArr);
-   } 
+  };
 
   return (
     <form>
       <div className="form-group">
+        <h4>Hit the refresh button below to get a new batch of EfficienCorp employees.</h4>
+        <button onClick={props.handleFormSubmit} className="btn btn-primary">
+          Refresh
+        </button>
+        <br />
+        <br />
+        <h5>Then feel free to search for an employee by first name.</h5>
         <label htmlFor="search">Search:</label>
         <input
-          onChange={(e)=> handleInputChange(e)}
+          onChange={(e) => handleInputChange(e)}
           type="text"
           className="form-control"
-          placeholder="Search For an EfficienCorp Employee"
+          placeholder="Search For an EfficienCorp Employee BY FIRST NAME"
         />
-        <br />
-        <button onClick={props.handleFormSubmit} className="btn btn-primary">
-          Search
-        </button>
       </div>
     </form>
   );
